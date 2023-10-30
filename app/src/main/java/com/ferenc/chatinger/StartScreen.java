e90cf7eddfe2a0d1b945cb2109f2c6f2f9901325
ParoniumIsotop137 <horwathslos29@gmail.com>
2023-10-30 13:43:54 +0100

    v0.1
    Startképernyő

---
+++ app/src/main/java/com/ferenc/chatinger/StartScreen.java
@@ -0,0 +1,26 @@
+package com.ferenc.chatinger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class StartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
