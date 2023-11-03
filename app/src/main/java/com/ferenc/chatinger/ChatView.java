b25c096cac6fa9e8211f4b19e2a756089c83c7cf
ParoniumIsotop137 <horwathslos29@gmail.com>
2023-11-03 13:37:59 +0100

    v0.8
    Üzenetküldés funkció

--- app/src/main/java/com/ferenc/chatinger/ChatView.java
+++ app/src/main/java/com/ferenc/chatinger/ChatView.java
@@ -3,10 +3,12 @@ package com.ferenc.chatinger;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,10 +17,14 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class ChatView extends AppCompatActivity {
@@ -33,6 +39,10 @@ public class ChatView extends AppCompatActivity {
 
     RecyclerView msgAdapter;
 
+    ArrayList<MessageModell> messageList;
+
+    MessageAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,21 +51,51 @@ public class ChatView extends AppCompatActivity {
         fauth = FirebaseAuth.getInstance();
         dBase = FirebaseDatabase.getInstance("https://chatinger-d3269-default-rtdb.europe-west1.firebasedatabase.app/");
 
+        messageList = new ArrayList<MessageModell>();
+
         partnerName = getIntent().getStringExtra("userName");
         partnerID = getIntent().getStringExtra("userID");
         senderID = fauth.getUid();
 
         btnSend = findViewById(R.id.btnSendChV);
         message = findViewById(R.id.txtMessage);
-        msgAdapter = findViewById(R.id.msgAdapter);
+
+        View view = LayoutInflater.from(ChatView.this).inflate(R.layout.activity_chat_view, null);
+        msgAdapter = view.findViewById(R.id.msgAdapterCHV);
+
 
         uName = findViewById(R.id.userNameChv);
         uName.setText(partnerName);
 
-        DatabaseReference chatReference = dBase.getReference().child("user").child(senderRoom).child("messages");
-
         senderRoom = senderID+partnerID;
         partnerRoom = partnerID+senderID;
+
+        DatabaseReference chatReference = dBase.getReference().child("user").child(senderRoom).child("messages");
+
+        LinearLayoutManager lyManager = new LinearLayoutManager(this);
+        lyManager.setStackFromEnd(true);
+        msgAdapter.setLayoutManager(lyManager);
+
+        adapter = new MessageAdapter(ChatView.this, messageList);
+        msgAdapter.setAdapter(adapter);
+
+        chatReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                messageList.clear();
+                for (DataSnapshot item : snapshot.getChildren()
+                     ) {
+                    MessageModell modellMessages = item.getValue(MessageModell.class);
+                    messageList.add(modellMessages);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
